570dee6ae60a3c4f1650a61e6ee70c19d6533986
Ascaliko <ascaliko7@gmail.com>
2019-10-03 03:59:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ console.log('number /= 5 // 1 ', number)
 
 
 
+
+function add(a, b){
+  return a + b
+}
+
+function subtract(a, b){
+  return a - b
+}
+
 function multiply(a, b){
   return a * b
 }
