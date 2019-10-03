9a50bc33da0cb4f14eb4e91bf6aee98ad22a343b
Ascaliko <ascaliko7@gmail.com>
2019-10-03 03:39:01 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var number = 15
+console.log(number++)
+console.log(number--)
+console.log(number)
+console.log(--number)
+console.log(++number)
+number += 3 // 8
+console.log('number += 3 // 8 ', number)
+number -= 2 // 3
+console.log('number -= 2 // 3 ', number)
+number *= 10 // 50
+console.log('number *= 10 // 50 ', number)
+number /= 5 // 1
+console.log('number /= 5 // 1 ', number)
